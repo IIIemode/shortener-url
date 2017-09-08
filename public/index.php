72b1a7b7a5b3b24fd1bf3c256a224d9b828cec78
DenisZhibenkov <deniszhibenkov@qip.ru>
2017-09-08 10:29:05 +0300

    Work with $_SESSION

--- public/index.php
+++ public/index.php
@@ -1,3 +1,6 @@
+<?php 
+    session_start();
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,7 +9,15 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta/css/bootstrap.min.css" integrity="sha384-/Y6pD6FV/Vv2HJnA6t+vslU6fwYXjCFtcEpHbNJ0lyAFsXTsjBbfaDjzALeQsN6M" crossorigin="anonymous">
 </head>
 <body>
-    <div id="app"></div>
+    <div class="container">
+        <div id="app"></div>
+        <?php 
+            if (isset($_SESSION['feedback'])) {
+                echo "<p>".$_SESSION['feedback']."</p>";
+                unset($_SESSION['feedback']);
+            }
+        ?>
+    </div>
 <script src="bundle.js"></script>
 </body>
 </html>
--- public/shorten.php
+++ public/shorten.php
@@ -1,26 +1,27 @@
-<?php 
-
+<?php
+    session_start();
     require_once 'classes/shortener.php';
 
     $s = new Shortener();
 
     if (isset($_POST['url'])) {
         $url = $_POST['url'];
-        echo $url;
 
-        $code = $s->makeCode($url);
+        if  ($code = $s->makeCode($url)) {
+            $_SESSION['feedback'] = "Done! Here is your link <a href='{$s->getUrl($code)}'>http://localhost:8080/Shortener_URL/public/$code</a>";
+        } else {
+            $_SESSION['feedback'] = "Error! Perhaps an incorrect URl?";
+        }
+        
+
     } else if (isset($_GET['codeOfUrl'])) {
         $massUrl = parse_url($_GET['codeOfUrl']);
-        
         $massUrl[path] = substr($massUrl[path], 22, strlen($massUrl[path]) - 22);
         $code = $massUrl[path];
-
         if ($url = $s->getUrl($code)){
-            // echo $url;
             header("Location: $url");
             exit();
         }
-
     }
-
+    header('location: index.php');
 ?>
