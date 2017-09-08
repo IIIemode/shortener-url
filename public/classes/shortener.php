133bc93224a02748dbd706995e88cc25ace68c3f
DenisZhibenkov <deniszhibenkov@qip.ru>
2017-09-08 13:05:22 +0300

    Validate if requested short url is not in use yet

--- public/classes/shortener.php
+++ public/classes/shortener.php
@@ -8,25 +8,42 @@
         }
 
         public function generateCode($num) {
+            // some generate code $num
+            $checkNum = $this->db->query("SELECT url FROM urls WHERE code = '$num'");
+            if ($checkNum->num_rows) {
+                $this->generateCode($num);
+            }
             return $num;     
         }
 
         public function makeCode($url) {
             $url = trim($url);
 
+            if (!filter_var($url, FILTER_VALIDATE_URL)) {
+                return '';
+            }
 
-            $this->db->query("INSERT INTO urls(url, created) VALUES('{$url}', NOW())");
-            $code = $this->generateCode($this->db->insert_id);
-            $this->db->query("UPDATE urls SET code='{$code}' WHERE url = '{$url}'");
-            return $code;
+            $url = $this->db->escape_string($url);
+            $exsists = $this->db->query("SELECT code FROM urls WHERE url='{$url}'");
+            if ($exsists->num_rows) {
+                return $exsists->fetch_object()->code;
+            } else {
+                $this->db->query("INSERT INTO urls(url, created) VALUES('{$url}', NOW())");
+                $code = $this->generateCode($this->db->insert_id);
+                $this->db->query("UPDATE urls SET code='{$code}' WHERE url = '{$url}'");
+                return $code;
+            }
         }
 
         public function getUrl($code){
-            
+            $code = $this->db->escape_string($code);
             $code = $this->db->query("SELECT url FROM urls WHERE code = '$code'");
 
-            return $code->fetch_object()->url;
+            if ($code->num_rows) {
+                return $code->fetch_object()->url;
+            }
+
+            return '';
         }
     }
-
 ?>
